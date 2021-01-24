d42b2de1faab46be17cb43795904d0a3b8ce56bd
punitsharma1009 <paras77295@gmail.com>
2021-01-25 02:54:48 +0530

    dff

---
+++ ARRAY/array sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int item;
+    cin>>item;
+    int price[item];
+    for(int i=0;i<item;i++){
+        cin>>price[i];
+    
+    }
+    // for(int i=0;i<digit;i++){
+    //     cout<<price[i];
+    // }
+    int sum=0; 
+    for(int i=0;i<item;i++){
+        sum=sum+price[i];
+    }
+	cout<<sum;
+	return 0;
+}
