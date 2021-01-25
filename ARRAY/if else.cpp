1a6f4e5804f809d4f64e3f3cbff6d68c464dc7d9
punitsharma1009 <paras77295@gmail.com>
2021-01-25 17:37:35 +0530

    if else

---
+++ ARRAY/if else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age;
+	cout<<"enter your age";
+	cin>>age;
+	if(age<=18)
+	cout<<"you are smart";
+	else
+	cout<<"you are ugly";
+	return 0;
+}
